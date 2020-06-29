88632849c621a74af59f533008d7d1af0192166a
hazuwall <39948232+Hazuwall@users.noreply.github.com>
2020-06-29 21:36:50 +0300

    Updates

---
+++ precache-manifest.4a356419067f0aa319ebb0bc325a7519.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd3aba6c6df7e08d59d3af89dc0b9501",
+    "url": "/sp500-performacne/index.html"
+  },
+  {
+    "revision": "13bf7342629d0f4ef207",
+    "url": "/sp500-performacne/static/css/main.17e8629c.chunk.css"
+  },
+  {
+    "revision": "6fe89b3a0597aaec5ef2",
+    "url": "/sp500-performacne/static/js/2.aced6d72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sp500-performacne/static/js/2.aced6d72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13bf7342629d0f4ef207",
+    "url": "/sp500-performacne/static/js/main.9dc8a64e.chunk.js"
+  },
+  {
+    "revision": "5b3b2cbd3a7b62a6b8cd",
+    "url": "/sp500-performacne/static/js/runtime-main.dfe51a69.js"
+  }
+]);
